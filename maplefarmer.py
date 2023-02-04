eb8b2e3aab5a4cb92179639a66e4e4703d791462
kwak9601 <kwak9601@gmail.com>
2023-02-04 02:59:26 -0700

    update: add more commands

--- maplefarmer.py
+++ maplefarmer.py
@@ -10,9 +10,14 @@ win.title("Maple Farmer")
 #Entire font 
 win.option_add("*Font","Timesnewroman 25")
 
-# Create button
+# Buttons
 btn = Button(win, text="Button")
 btn.pack()
+btn.config(width=20, height=10)
+btn.config(text = 'PRESS')
+def alert():
+    print('button is pressed')
+btn.config(command=alert)
 
 # change bg color
 win.configure(bg='light blue')
